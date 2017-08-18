fb6fcffc45dddda116b949d90d664107f79fe593
thanhbv <thanhbv@sandinh.net>
2017-08-18 14:40:16 +0700

    reduce quill select data

--- src/main/scala/com/sandinh/xdi/model/XfAttachmentData.scala
+++ src/main/scala/com/sandinh/xdi/model/XfAttachmentData.scala
@@ -2,12 +2,14 @@ package com.sandinh.xdi.model
 
 case class XfAttachmentData(dataId: Int,
                             userId: Int,
-                            uploadDate: Int,
-                            filename: String,
-                            fileSize: Int, fileHash: String,
+//                            uploadDate: Int,
+//                            filename: String,
+//                            fileSize: Int,
+                            fileHash: String,
                             filePath: String,
-                            width: Int,
-                            height: Int,
-                            thumbnailWidth: Int,
-                            thumbnailHeight: Int,
-                            attachCount: Int)
+//                            width: Int,
+//                            height: Int,
+//                            thumbnailWidth: Int,
+//                            thumbnailHeight: Int,
+//                            attachCount: Int
+                           )
--- src/main/scala/com/sandinh/xdi/model/XfUser.scala
+++ src/main/scala/com/sandinh/xdi/model/XfUser.scala
@@ -1,14 +1,17 @@
 package com.sandinh.xdi.model
 
-case class XfUser(userId: Int, username: String, email: String,
-                  gender: String, //'', 'male', 'female'
-                  customTitle: String, languageId: Int, styleId: Int, timezone: String, visible: Byte,
-                  activityVisible: Byte, userGroup_id: Int,
-                  secondaryGroupIds: Array[Byte],
-                  displayStyleGroupId: Int, permissionCombinationId: Int,
-                  messageCount: Int, conversationsUnread: Short, registerDate: Int, lastActivity: Int,
-                  trophyPoints: Int, alertsUnread: Short,
-                  avatarDate: Int, avatarWidth: Short, avatarHeight: Short, gravatar: String,
-                  userState: String, //'valid', 'email_confirm', 'email_confirm_edit', 'moderated', 'email_bounce'
-                  isModerator: Byte, isAdmin: Byte, isBanned: Byte,
-                  likeCount: Int, warningPoints: Int, isStaff: Byte)
+case class XfUser(userId: Int,
+//                  username: String, email: String,
+//                  gender: String, //'', 'male', 'female'
+//                  customTitle: String, languageId: Int, styleId: Int, timezone: String, visible: Byte,
+//                  activityVisible: Byte, userGroup_id: Int,
+//                  secondaryGroupIds: Array[Byte],
+//                  displayStyleGroupId: Int, permissionCombinationId: Int,
+//                  messageCount: Int, conversationsUnread: Short, registerDate: Int, lastActivity: Int,
+//                  trophyPoints: Int, alertsUnread: Short,
+                  avatarDate: Int
+//                  avatarWidth: Short, avatarHeight: Short, gravatar: String,
+//                  userState: String, //'valid', 'email_confirm', 'email_confirm_edit', 'moderated', 'email_bounce'
+//                  isModerator: Byte, isAdmin: Byte, isBanned: Byte,
+//                  likeCount: Int, warningPoints: Int, isStaff: Byte
+                 )
